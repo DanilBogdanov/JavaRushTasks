71844844a6d5ad81435acd329f58829fa81039bf
DanilKardon <dangerdanil@gmail.com>
2019-04-23 18:17:43 +0500

    2201

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2106/Test.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2106/Test.java
@@ -3,35 +3,19 @@ package com.javarush.task.task21.task2106;
 
 
 public class Test implements Cloneable {
-    public static class User implements Cloneable {
-        int age;
-        String name;
-
-        public User(int age, String name) {
-            this.age = age;
-            this.name = name;
-        }
-
-        @Override
-        public User clone() throws CloneNotSupportedException {
-            User user = (User) super.clone();
-
-            return user;
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         String a = "a";
-        String b = "b";
+        String b = a;
+        StringBuffer aa = new StringBuffer("abuf");
+        StringBuffer bb = aa;
+        aa.append(1);
+        a = "aa";
 
-        String [] array = new String[]{a, b};
-        String [] clone = array.clone();
-
-        array[1] = "adsf";
-        a = "asdfasdfasdf";
-        for(String s : clone) {
-            System.out.println(s);
-        }
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(aa);
+        System.out.println(bb);
     }
 
 
---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2201/StringForFirstThreadTooShortException.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task22.task2201;
+
+public class StringForFirstThreadTooShortException extends RuntimeException {
+    public StringForFirstThreadTooShortException(Throwable cause) {
+        super(cause);
+    }
+
+    @Override
+    public synchronized Throwable getCause() {
+        return new StringIndexOutOfBoundsException("String index out of range: -1");
+    }
+}
