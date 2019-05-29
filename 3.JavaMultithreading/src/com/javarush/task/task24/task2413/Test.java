5832c0c856f6a81529ab2217b519c6639d4e8599
DanilKardon <dangerdanil@gmail.com>
2019-05-29 23:53:56 +0500

    2407

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Test.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Test.java
@@ -1,9 +1,25 @@
 package com.javarush.task.task24.task2413;
 
-public class Test {
+public class Test implements Cloneable {
 
     public static void main(String[] args) {
-        System.out.println((int) 1.23);
-        System.out.println((int) -1.78);
+        Test test = new Test();
+        Inner inner = (new Test()).new Inner();
+
+        Test t = test.clone();
+    }
+
+    @Override
+    public Test clone() {
+        try {
+            return (Test) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+
+    public class Inner {
+        int i;
+
     }
 }
