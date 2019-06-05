8557feac25043e9ad4f4fac876ee18d4dfc5cea0
DanilKardon <dangerdanil@gmail.com>
2019-06-05 23:56:58 +0500

    2503

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() {
+            //todo wheels = new ArrayList<>();
+            ArrayList<Wheel> wheelsMock = new ArrayList<>();
+            for (String wheel : loadWheelNamesFromDB()) {
+                wheelsMock.add(Wheel.valueOf(wheel));
+            }
+            if (wheelsMock.size() == Wheel.values().length &&
+                wheelsMock.containsAll(Arrays.asList(Wheel.values()))) {
+                wheels = wheelsMock;
+            } else {
+                throw new RuntimeException("fuck"); Thread
+            }
+
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+        new Car();
+    }
+}
