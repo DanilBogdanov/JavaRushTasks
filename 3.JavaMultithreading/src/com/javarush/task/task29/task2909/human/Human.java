9544cbf9dc72f81eec053e385bd2150876a0c1d4
DanilKardon <dangerdanil@gmail.com>
2019-06-17 00:19:31 +0500

    2909 in progress(2)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -4,25 +4,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Human implements Alive{
-    public static int nextId = 0;
+public class Human implements Alive {
+    private static int nextId = 0;
     private int id;
     protected int age;
     protected String name;
-    protected int[] size;
+    protected Size size;
     private List<Human> children = new ArrayList<>();
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
-    public int getBloodGroup() {
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
@@ -77,11 +73,12 @@ public class Human implements Alive{
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void printSize() {
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
-    public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+    public class Size {
+        public int height;
+        public int weight;
     }
 }
