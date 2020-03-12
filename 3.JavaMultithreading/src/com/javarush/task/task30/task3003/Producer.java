6531e83cf0c76fe0b17e16b66745059fdf7c631f
Danil <dangerdanil@gmail.com>
2020-03-12 21:25:00 +0500

    12 mar 2020

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3003/Producer.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task30.task3003;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TransferQueue;
+
+public class Producer implements Runnable {
+    TransferQueue<ShareItem> queue;
+
+    public Producer(TransferQueue<ShareItem> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            for (int i = 1; i <= 9; i++) {
+                System.out.format("Элемент 'ShareItem-%d' добавлен%n", i);
+                queue.offer(new ShareItem("ShareItem-" + i, i));
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    return;
+                }
+                if (queue.hasWaitingConsumer()) {
+                    System.out.format("Consumer в ожидании!%n");
+                }
+            }
+            return;
+        }
+    }
+}
