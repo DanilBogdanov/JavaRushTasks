ae90eb49d7a08ad08450f9f815709605f811bd87
Danil <dangerdanil@gmail.com>
2020-05-03 23:39:20 +0500

    3 may 2020

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Test.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Test.java
@@ -2,15 +2,15 @@ package com.javarush.task.task31.task3110;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        FileManager fm = new FileManager(Paths.get("/home/danil/test"));
-        List<Path> list = fm.getFileList();
+        Path zip = Paths.get("/home/danil/test/1.zip");
+        Path path2 = Paths.get("/home/danil/test/2.txt");
 
-        for (Path path : list) {
-            System.out.println(path);
-        }
+        ZipFileManager zfm = new ZipFileManager(zip);
+        zfm.addFile(path2);
     }
 }
