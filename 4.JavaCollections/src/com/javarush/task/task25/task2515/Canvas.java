587c09b23d0f6838fcae3c7490d562056734a754
Danil <dangerdanil@gmail.com>
2020-05-28 22:01:53 +0500

    23 may 2020 home

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -1,48 +1,55 @@
 package com.javarush.task.task25.task2515;
-/*Что мы будем делать с Canvas?
-Поэтому нам понадобятся два метода:
-public void setPoint(double x, double y, char c),
-public void drawMatrix(double x, double y, int[][] matrix, char c).
 
-Первый метод - setPoint будет "ставить точку в координатах x,y цветом c".
-В методе надо:
-а) округлить x и y до целых чисел,
-б) занести в matrix[y][x] значение с,
-в) ничего не делать, если x<0 или y<0 или y>=matrix.length или x>=matrix[0].length.
 
-Второй метод - drawMatrix копирует переданную ему картинку (матрицу) в матрицу Canvas.
-И не просто копирует, а начиная с координат x, y.
-
-В методе надо:
-а) с помощью двух вложенных циклов пройтись по всем ячейкам переданной картинки,
-б) если значение ячейки matrix[i][j] не равно 0, то покрасить в матрице объекта Canvas точку (x+j, y+i) в цвет c:
-setPoint(x+j, y+i, c)
-*/
 public class Canvas {
     private int width;
     private int height;
     private char[][] matrix;
 
-    public void setPoint(double x, double y, char c) {
-        if (x >= 0 && y >= 0 && x < matrix[0].length && y < matrix.length) {
-            matrix[(int) Math.round(y)][(int) Math.round(x)] = c;
-        }
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    public void clear() {
+        this.matrix = new char[height + 2][width + 2];
     }
 
     public void drawMatrix(double x, double y, int[][] matrix, char c) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] != 0) {
-                    setPoint(j + x,i + y, c);
+                    setPoint(x + j, y + i, c);
                 }
             }
         }
     }
 
-    public Canvas(int width, int height) {
-        this.width = width;
-        this.height = height;
-        this.matrix = new char[height][width];
+    public void setPoint(double x, double y, char c) {
+        int xRounded = (int) Math.round(x);
+        int yRounded = (int) Math.round(y);
+        if (xRounded >= 0 && xRounded < matrix[0].length && yRounded >= 0 && yRounded < matrix.length) {
+            matrix[yRounded][xRounded] = c;
+        }
+    }
+
+    public void print() {
+        System.out.println();
+
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
     public int getWidth() {
