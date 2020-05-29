ac8f6feac59fe822e7b6b77b8d5ca112ab52ba53
Danil <dangerdanil@gmail.com>
2020-05-29 19:52:28 +0500

    29 may 2020 home finish big task mvc

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -9,7 +9,12 @@ public class UsersView implements View {
 
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All users:");
+        if (modelData.isDisplayDeletedUserList()) {
+            System.out.println("All deleted users:");
+        } else {
+            System.out.println("All users:");
+        }
+
         for (User user : modelData.getUsers()) {
             System.out.println("\t" + user);
         }
@@ -27,5 +32,12 @@ public class UsersView implements View {
 
     public void fireEventShowDeletedUsers() {
         controller.onShowAllDeletedUsers();
+
     }
+
+    public void fireEventOpenUserEditForm(long id) {
+        controller.onOpenUserEditForm(id);
+    }
+
+
 }
